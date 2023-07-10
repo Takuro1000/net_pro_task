0ce2443d62ff1410295c16419efc092c2399d571
Takuro1000 <taku.0116.19@gmail.com>
2023-07-10 12:02:29 +0900

    del problems

--- TeamTask/thread/test2.java
+++ TeamTask/thread/test2.java
@@ -2,11 +2,7 @@ package TeamTask.thread;
 
 import java.net.Socket;
 import java.net.ServerSocket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
